8e9d9c8cc3b37826ee3eafe008f3be28b87b4b7b
Josie Welsh <jowe9773@colorado.edu>
2024-07-09 13:46:32 -0500

    adding file managers file with class

---
+++ file_managers.py
@@ -0,0 +1,42 @@
+#file_managers.py
+
+"""module containing methods for managing files"""
+
+#import necessary packages
+import tkinter as tk
+from tkinter import filedialog
+
+
+class FileManagers:
+    """Class contains methods for managing files"""
+
+    def __init__(self):
+        print("initialized")
+
+    def load_dn(self, purpose):
+        """this function opens a tkinter GUI for selecting a 
+        directory and returns the full path to the directory 
+        once selected
+        
+        'purpose' -- provides expanatory text in the GUI
+        that tells the user what directory to select"""
+
+        root = tk.Tk()
+        root.withdraw()
+        directory_name = filedialog.askdirectory(title = purpose)
+
+        return directory_name
+
+    def load_fn(self, purpose):
+        """this function opens a tkinter GUI for selecting a 
+        file and returns the full path to the file 
+        once selected
+        
+        'purpose' -- provides expanatory text in the GUI
+        that tells the user what file to select"""
+
+        root = tk.Tk()
+        root.withdraw()
+        filename = filedialog.askopenfilename(title = purpose)
+
+        return filename
