1703b106109293969f96d242db134be80148112b
Josie Welsh <jowe9773@colorado.edu>
2024-07-11 08:44:20 -0500

    adding audiosync functionality to the merge videos script

--- merge_videos.py
+++ merge_videos.py
@@ -31,9 +31,18 @@ gcps_list = [gcps, gcps1, gcps2, gcps3]
 print(gcps_list)
 
 #time offsets between the videos (in ms)
+audio1, rate1 = ot.extract_audio(videos[0])
+audio2, rate2 = ot.extract_audio(videos[1])
+audio3, rate3 = ot.extract_audio(videos[2])
+audio4, rate4 = ot.extract_audio(videos[3])
+
+offset2 = ot.find_time_offset(rate1, audio1, rate2, audio2)
+offset3 = ot.find_time_offset(rate1, audio1, rate3, audio3)
+offset4 = ot.find_time_offset(rate1, audio1, rate4, audio4)
 
 
-offsets = [0, 4416.0, 10368.0, 19050.666666666668]
+offsets = [0, offset2*-1000, offset3*-1000, offset4*-1000]
+print(offsets)
 
 #choose start time for first video and length to process
 start = 305
